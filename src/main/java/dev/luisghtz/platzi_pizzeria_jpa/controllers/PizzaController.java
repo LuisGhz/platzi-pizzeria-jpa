b4264e1f2806d73dc80cd1359cf2c03fa0d95130
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-06-29 14:03:36 -0600

    Create PizzaController

---
+++ src/main/java/dev/luisghtz/platzi_pizzeria_jpa/controllers/PizzaController.java
@@ -0,0 +1,24 @@
+package dev.luisghtz.platzi_pizzeria_jpa.controllers;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.luisghtz.platzi_pizzeria_jpa.percistence.entity.PizzaEntity;
+import dev.luisghtz.platzi_pizzeria_jpa.services.PizzaService;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/pizzas")
+@RequiredArgsConstructor
+public class PizzaController {
+  private final PizzaService pizzaService;
+
+  @GetMapping
+  public ResponseEntity<List<PizzaEntity>> getAll() {
+    return ResponseEntity.ok(pizzaService.getAll());
+  }
+}
