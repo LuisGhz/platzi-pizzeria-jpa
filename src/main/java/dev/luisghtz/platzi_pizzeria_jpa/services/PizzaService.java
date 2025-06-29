b22341f4b4f6ef359677cfffcf8651762e58e6ec
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-06-29 14:29:42 -0600

    Use repository instead of jdbc template

--- src/main/java/dev/luisghtz/platzi_pizzeria_jpa/services/PizzaService.java
+++ src/main/java/dev/luisghtz/platzi_pizzeria_jpa/services/PizzaService.java
@@ -2,19 +2,23 @@ package dev.luisghtz.platzi_pizzeria_jpa.services;
 
 import java.util.List;
 
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
+// import org.springframework.jdbc.core.BeanPropertyRowMapper;
+// import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import dev.luisghtz.platzi_pizzeria_jpa.percistence.entity.PizzaEntity;
+import dev.luisghtz.platzi_pizzeria_jpa.percistence.repository.PizzaRepository;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class PizzaService {
-  private final JdbcTemplate jdbcTemplate;
+  // private final JdbcTemplate jdbcTemplate;
+  private final PizzaRepository pizzaRepository;
 
   public List<PizzaEntity> getAll() {
-    return this.jdbcTemplate.query("SELECT * FROM pizza", new BeanPropertyRowMapper<>(PizzaEntity.class));
+    return (List<PizzaEntity>) pizzaRepository.findAll();
+    // return this.jdbcTemplate.query("SELECT * FROM pizza", new
+    // BeanPropertyRowMapper<>(PizzaEntity.class));
   }
 }
