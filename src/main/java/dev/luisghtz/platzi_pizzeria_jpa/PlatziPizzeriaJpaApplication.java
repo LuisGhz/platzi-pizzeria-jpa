851af43f06d4ed323a35b4ceb91adeb00585dbe6
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-06-29 10:52:11 -0600

    Init project

---
+++ src/main/java/dev/luisghtz/platzi_pizzeria_jpa/PlatziPizzeriaJpaApplication.java
@@ -0,0 +1,13 @@
+package dev.luisghtz.platzi_pizzeria_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziPizzeriaJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziPizzeriaJpaApplication.class, args);
+	}
+
+}
