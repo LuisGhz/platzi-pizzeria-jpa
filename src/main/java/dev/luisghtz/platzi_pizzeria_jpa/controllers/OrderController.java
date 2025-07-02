e27dfbf9ddb0d7ff76e8e6bd233661fedd45867a
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-07-02 14:58:11 -0600

    Create OrderController

---
+++ src/main/java/dev/luisghtz/platzi_pizzeria_jpa/controllers/OrderController.java
@@ -0,0 +1,26 @@
+package dev.luisghtz.platzi_pizzeria_jpa.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.luisghtz.platzi_pizzeria_jpa.percistence.entity.OrderEntity;
+import dev.luisghtz.platzi_pizzeria_jpa.services.OrderService;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping; 
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+public class OrderController {
+  private final OrderService orderService;
+
+  @GetMapping()
+  public ResponseEntity<List<OrderEntity>> getMethodName() {
+    return ResponseEntity.ok(orderService.getAll());
+  }
+
+}
