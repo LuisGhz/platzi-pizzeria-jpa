02656d254bf5e4d8b9220dbaf97b054586cdaee2
Luis Antonio Gutiérrez Hernández <luisghtz@outlook.com>
2025-07-03 17:12:20 -0600

    Enable jpa auditing

--- src/main/java/dev/luisghtz/platzi_pizzeria_jpa/PlatziPizzeriaJpaApplication.java
+++ src/main/java/dev/luisghtz/platzi_pizzeria_jpa/PlatziPizzeriaJpaApplication.java
@@ -2,8 +2,10 @@ package dev.luisghtz.platzi_pizzeria_jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PlatziPizzeriaJpaApplication {
 
 	public static void main(String[] args) {
